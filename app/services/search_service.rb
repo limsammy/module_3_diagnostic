aaac08b7300820b9a66980e93e93fd017be7f833
limsammy1@gmail.com <limsammy1@gmail.com>
2017-12-13 10:08:31 -0700

    Updated to use nearest

--- app/services/search_service.rb
+++ app/services/search_service.rb
@@ -3,7 +3,7 @@ class SearchService
   def initialize(zip)
     @zip = zip
     api_key = 'K0WSG1pYDhLiVdIwBQh0gw60DJmKWhle5xa6NE4g'
-    @conn = Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?zip=#{zip}&limit=10&api_key=#{api_key}&format=JSON") do |faraday|
+    @conn = Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location=#{zip}&limit=10&api_key=#{api_key}&format=JSON") do |faraday|
       faraday.adapter Faraday.default_adapter
     end
   end
